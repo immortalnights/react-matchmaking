6711bb4c4abed23a02db0cc56c16fbaa35d52f33
Andrew <github@seventh.space>
2020-03-05 14:53:57 +0000

    add hookrouter and implement basic routes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-matchmaking
+
+
+### Help from
+https://github.com/philnash/react-express-starter
+https://github.com/esausilva/example-create-react-app-express
--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useRoutes } from 'hookrouter';
+
+const routes = {
+	'/': () => (<h1>Home</h1>),
+	'/Lobby': () => (<h1>Invalid lobby ID</h1>),
+	'/Lobby/:id': ({id}) => (<h1>Lobby for {id}</h1>),
+	'/Game': () => (<h1>Invalid game ID</h1>),
+	'/Game/:id': ({id}) => (<h1>Lobby for {id}</h1>),
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const content = useRoutes(routes) || (<h1>404</h1>);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				{content}
+			</header>
+		</div>
+	);
 }
 
 export default App;
