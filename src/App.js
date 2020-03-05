b17e3e71ddcd32af045303d8ebf6420e8d743af6
Andrew <github@seventh.space>
2020-03-05 16:37:29 +0000

    add Lobby and Game classes with common Loader component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,120 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useRoutes } from 'hookrouter';
+import { A, useRoutes } from 'hookrouter';
+
+const withLoader = (WrapperComponent, makeRequest) => {
+	return class Loader extends React.Component {
+		state = {
+			status: 'LOADING',
+			error: null,
+			data: null
+		};
+
+		request = null;
+
+		componentDidMount()
+		{
+			this.request = makeRequest(this.props.id).then(({ status, error, data }) => {
+				this.request = null;
+				this.setState({ status, error, data });
+			});
+		}
+
+		componentWillUnmount()
+		{
+			if (this.request)
+			{
+				this.request.cancel();
+			}
+		}
+
+		render()
+		{
+			let content;
+
+			switch (this.state.status)
+			{
+				case 'LOADING':
+				{
+					content = (<h1>Connecting...</h1>);
+					break;
+				}
+				case 'ERROR':
+				{
+					content = (<>
+						<h1>{this.state.error}</h1>
+						<A href="/">Return</A>
+					</>);
+					break;
+				}
+				default:
+				{
+					content = (<WrapperComponent {...this.props} data={this.state.data} />);
+					break;
+				}
+			}
+
+			return content;
+		}
+	}
+}
+
+class Lobby extends React.Component {
+	render()
+	{
+		return (<h1>In lobby</h1>);
+	}
+}
+
+const LobbyWithLoader = withLoader(Lobby, (id) => {
+	return fetch('/api/lobby/' + id).then((response) => {
+		const data = { status: 'ERROR', error: '', data: null };
+
+		if (response.ok)
+		{
+			data.status = 'IN_LOBBY';
+			data.data = response.responseJSON;
+		}
+		else
+		{
+			data.error = "Failed to load game lobby.";
+		}
+
+		return data;
+	});
+});
+
+class Game extends React.Component {
+	render()
+	{
+		return (<h1>In game</h1>);
+	}
+}
+
+const GameWithLoader = withLoader(Game, (id) => {
+	return fetch('/api/game/' + id).then((response) => {
+		const data = { status: 'ERROR', error: '', data: null };
+
+		if (response.ok)
+		{
+			data.status = 'IN_GAME';
+			data.data = response.responseJSON;
+		}
+		else
+		{
+			data.error = "Failed to load game lobby.";
+		}
+
+		return data;
+	});
+});
+
 
 const routes = {
 	'/': () => (<h1>Home</h1>),
-	'/Lobby': () => (<h1>Invalid lobby ID</h1>),
-	'/Lobby/:id': ({id}) => (<h1>Lobby for {id}</h1>),
-	'/Game': () => (<h1>Invalid game ID</h1>),
-	'/Game/:id': ({id}) => (<h1>Lobby for {id}</h1>),
+	'/Lobby/:id': ({id}) => (<LobbyWithLoader id={id} />),
+	'/Game/:id': ({id}) => (<GameWithLoader id={id} />)
 }
 
 function App() {
