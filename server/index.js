072e46a30fbf542ed3c565a1b513ad7658cff617
immortalnights <github@seventh.space>
2020-05-06 00:21:09 +0100

    move multiplayer game server files to immortalnights/multiplayer-game-server

--- server/index.js
+++
@@ -1,5 +0,0 @@
-const Server = require('./server');
-const Game = require('./game');
-
-const s = new Server({ createGame: (options) => { return new Game(options); } });
-s.start(process.env.port || 3001);
